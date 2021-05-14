f5470dee67fd08aa43b57258876dae2ab0b0c401
Jorge Mendoza <jorge@dynamiteapps.io>
2021-05-14 18:10:03 +0200

    Testing new linking schema

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export default [
   {
     name:'New Listing',
-    link:'https://dev.avenue8.com/clientlist&cntrll=ClientsRouter'
+    link:'https://dev.avenue8.com/clientlist?cntrll=ClientsRouter'
   },
   {
     name:'Content Library',
