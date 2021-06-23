bc527e2177a383e462131f95ce6aa30f04325536
Jorge Mendoza <jorge@dynamiteapps.io>
2021-06-23 13:54:19 +0200

    reverted dl address

--- src/data.js
+++ src/data.js
@@ -1,55 +1,55 @@
 export default [
   {
     name:'Client Listing',
-    link:'https://link.avenue8.com/?c=ClientsRouter'
+    link:'https://dev.avenue8.com/?c=ClientsRouter'
   },
   {
     name:'Client Listing nickname',
-    link:'https://link.avenue8.com/?c=AF112233'
+    link:'https://dev.avenue8.com/?c=AF112233'
   },
   {
     name:'Client Details (Correct Client)',
-    link:'https://link.avenue8.com/?c=ClientDetailRouter&id=2111'
+    link:'https://dev.avenue8.com/?c=ClientDetailRouter&id=2111'
   },
   {
     name:'Client Details (Incorrect Client)',
-    link:'https://link.avenue8.com/?c=ClientDetailRouter&id=109'
+    link:'https://dev.avenue8.com/?c=ClientDetailRouter&id=109'
   },
   {
     name:'Client Details (wrong controller)',
-    link:'https://link.avenue8.com/?c=ShoesController&id=109'
+    link:'https://dev.avenue8.com/?c=ShoesController&id=109'
   },
   {
     name:'Active Listings',
-    link:'https://link.avenue8.com/?c=ActiveListingRouter'
+    link:'https://dev.avenue8.com/?c=ActiveListingRouter'
   },
   {
     name:'Listing Details by ID',
-    link:'https://link.avenue8.com/?c=ActiveListingDetailRouter&id=138'
+    link:'https://dev.avenue8.com/?c=ActiveListingDetailRouter&id=138'
   },
   {
     name:'Content Library',
-    link:'https://link.avenue8.com/?c=ContentLibraryRouter'
+    link:'https://dev.avenue8.com/?c=ContentLibraryRouter'
   },
   {
     name:'CMA with Address',
-    link:'https://link.avenue8.com/?c=CMARouter&listingBeds=2&listingLivingSpace=2000&listingAddressLine2=&listingCity=San%20Francisco&clientEmail=jorgevmendoza+testingavenue8email@gmail.com&listingAddressLine1=2333%20Buchanan%20Street&clientLastname=felipe&clientFirstname=king&listingBaths=2.0&listingState=CA&clientId=167&listingZip=94115'
+    link:'https://dev.avenue8.com/?c=CMARouter&listingBeds=2&listingLivingSpace=2000&listingAddressLine2=&listingCity=San%20Francisco&clientEmail=jorgevmendoza+testingavenue8email@gmail.com&listingAddressLine1=2333%20Buchanan%20Street&clientLastname=felipe&clientFirstname=king&listingBaths=2.0&listingState=CA&clientId=167&listingZip=94115'
   },
   {
     name:'CMA with Client',
-    link:'https://link.avenue8.com/?c=CMARouter&clientEmail=jorgevmendoza+testingavenue8email@gmail.com&clientFirstname=king&clientLastname=felipe'
+    link:'https://dev.avenue8.com/?c=CMARouter&clientEmail=jorgevmendoza+testingavenue8email@gmail.com&clientFirstname=king&clientLastname=felipe'
   },
   {
     name:'Visual Marketing with Listing',
-    link:'https://link.avenue8.com/?c=VisualMarketingActiveListingRouter&listingId=138'
+    link:'https://dev.avenue8.com/?c=VisualMarketingActiveListingRouter&listingId=138'
   },
   {
     name:'Registration with data',
-    link:'https://link.avenue8.com/?c=RegisterRouter&firstname=Clark&lastname=Kent&email=superman@avenue8.com&phone=405343334'
+    link:'https://dev.avenue8.com/?c=RegisterRouter&firstname=Clark&lastname=Kent&email=superman@avenue8.com&phone=405343334'
   }
   ,
   {
     name:'Forgot password with data',
-    link:'https://link.avenue8.com/?c=RecoveryPasswordRouter&username=superman@avenue8.com'
+    link:'https://dev.avenue8.com/?c=RecoveryPasswordRouter&username=superman@avenue8.com'
   }
 ]
